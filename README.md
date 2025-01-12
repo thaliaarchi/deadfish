b5a2dc8d611d8ffab947dcef6ade0515a1bc7e78
Thalia Archibald <thalia@archibald.dev>
2025-01-12 06:35:47 -0800

    Relicense under MPL-2.0

--- README.md
+++ README.md
@@ -2,11 +2,3 @@
 
 `deadfish` is an efficient encoder for the [Deadfish](https://esolangs.org/wiki/Deadfish)
 programming language, with accurate semantics.
-
-## License
-
-`deadfish` is released by Thalia Archibald under the terms of the
-[GNU Lesser General Public License](https://www.gnu.org/licenses/lgpl-3.0.html),
-version 3 or later. This license makes the it free to use, share, and improve,
-with the requirement that any alterations or improvements to `deadfish` also
-must be released as open source under this same license.
--- src/builder.rs
+++ src/builder.rs
@@ -1,11 +1,3 @@
-// Copyright (C) 2022 Thalia Archibald
-//
-// deadfish is free software: you can redistribute it and/or modify it under the
-// terms of the GNU Lesser General Public License as published by the Free
-// Software Foundation, either version 3 of the License, or (at your option) any
-// later version. You should have received a copy of the GNU Lesser General
-// Public License along with deadfish. If not, see http://www.gnu.org/licenses/.
-
 use std::collections::VecDeque;
 
 use crate::{heuristic_encode, Inst, Offset, Value};
